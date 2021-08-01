066a923545ddbea917700007ee899925d6a5ade6
luke <luc.dewit@hotmail.nl>
2021-08-01 15:58:15 +0200

    add working navigation

--- src/router.js
+++
@@ -1,9 +0,0 @@
-import NotFound from "./views/NotFound.vue"
-import Home from "./views/Home.vue"
-
-const routes = [
-    { path: "/", component: Home },
-    { path: "*", component: NotFound }
-];
-
-export default routes;
